fce7df4ed997cfbe9ae60674ca8565e34d8d291c
ValentinPIVOT-LEMEEetpa <pivot.valentin@laposte.net>
2020-01-13 16:55:32 +0100

    ex2
    boucle qui fait perdre les points de vie jusqu'à 0

--- jeuTP1.c
+++ jeuTP1.c
@@ -8,11 +8,15 @@ int poison= 15;
 int mains= 1;
 
 int main(){
-
 printf("vie:%d\n", vie);
+while(vie!=0){
 
+printf("\n");
 printf("Tu t'es pris un coup par une arme\n");
 	vie= vie-armes;
-printf("vie:%d\n", vie);
 
+printf("vie:%d\n", vie);
+};
+
+return 0;
 }
