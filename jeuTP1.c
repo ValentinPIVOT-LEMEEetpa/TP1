b440e8add3ad7abd147614fc051c8ff3cd39d6c4
ValentinPIVOT-LEMEEetpa <pivot.valentin@laposte.net>
2020-01-13 17:35:08 +0100

    ex3
    boucle pour perdre la vie jusqu'à 0

--- jeuTP1.c
+++ jeuTP1.c
@@ -2,21 +2,36 @@
 
 
 int vie=100;
+int maVie=100;
 
 int armes=5;
 int poison= 15;
 int mains= 1;
 
 int main(){
-printf("vie:%d\n", vie);
-while(vie!=0){
+printf("sa vie:%d\n", vie);
+printf("ma vie:%d\n", maVie);
+
+while(vie >0 && maVie >0){
 
 printf("\n");
-printf("Tu t'es pris un coup par une arme\n");
+printf("Tu t'es pris un coup par une arme. Tu pers %d points de vie\n" , armes);
 	vie= vie-armes;
 
-printf("vie:%d\n", vie);
+printf("Sa vie est de:%d\n", vie);
+
+printf("\n");
+printf("Je me suis pris un coup par une arme. Je pers %d points de vie\n" , poison);
+	maVie= maVie-poison;
+
+printf("ma vie est de :%d\n", maVie);
 };
 
+
+
+
 return 0;
+
+
+
 }
